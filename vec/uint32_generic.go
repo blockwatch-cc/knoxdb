750e7d3e345e2005ef4ac28fcc942d9a747abc5d
Stefan Schramm <stefan@blockwatch.cc>
2020-07-28 02:36:16 +0200

    added generic MatchEqual method for uint32

---
+++ vec/uint32_generic.go
@@ -0,0 +1,83 @@
+// Copyright (c) 2020 Blockwatch Data Inc.
+// Author: stefan@blockwatch.cc
+
+package vec
+
+func matchUint32EqualGeneric(src []uint32, val uint32, bits []byte) int64 {
+	var cnt int64
+	for i, v := range src {
+		if v == val {
+			bits[i>>3] |= 0x1 << uint(7-i&0x7)
+			cnt++
+		}
+	}
+	return cnt
+}
+/*
+func matchUint64NotEqualGeneric(src []uint64, val uint64, bits []byte) int64 {
+	var cnt int64
+	for i, v := range src {
+		if v != val {
+			bits[i>>3] |= 0x1 << uint(7-i&0x7)
+			cnt++
+		}
+	}
+	return cnt
+}
+
+func matchUint64LessThanGeneric(src []uint64, val uint64, bits []byte) int64 {
+	var cnt int64
+	for i, v := range src {
+		if v < val {
+			bits[i>>3] |= 0x1 << uint(7-i&0x7)
+			cnt++
+		}
+	}
+	return cnt
+}
+
+func matchUint64LessThanEqualGeneric(src []uint64, val uint64, bits []byte) int64 {
+	var cnt int64
+	for i, v := range src {
+		if v <= val {
+			bits[i>>3] |= 0x1 << uint(7-i&0x7)
+			cnt++
+		}
+	}
+	return cnt
+}
+
+func matchUint64GreaterThanGeneric(src []uint64, val uint64, bits []byte) int64 {
+	var cnt int64
+	for i, v := range src {
+		if v > val {
+			bits[i>>3] |= 0x1 << uint(7-i&0x7)
+			cnt++
+		}
+	}
+	return cnt
+}
+
+func matchUint64GreaterThanEqualGeneric(src []uint64, val uint64, bits []byte) int64 {
+	var cnt int64
+	for i, v := range src {
+		if v >= val {
+			bits[i>>3] |= 0x1 << uint(7-i&0x7)
+			cnt++
+		}
+	}
+	return cnt
+}
+
+func matchUint64BetweenGeneric(src []uint64, a, b uint64, bits []byte) int64 {
+	diff := b - a + 1
+	var cnt int64
+	for i, v := range src {
+		if v-a < diff {
+			bits[i>>3] |= 0x1 << uint(7-i&0x7)
+			cnt++
+		}
+	}
+	return cnt
+}
+*/
