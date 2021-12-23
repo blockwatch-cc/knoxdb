ad6ea3fe9352e1a7808db595405de4f0dcdaeb99
Alexander Eichhorn <alex@blockwatch.cc>
2021-12-23 13:16:06 +0400

    Add missing ARM64 handlers

---
+++ vec/float32_ref.go
@@ -0,0 +1,35 @@
+// Copyright (c) 2020 Blockwatch Data Inc.
+// Author: alex@blockwatch.cc
+
+//go:build !amd64 || appengine || gccgo
+// +build !amd64 appengine gccgo
+
+package vec
+
+func matchFloat32Equal(src []float32, val float32, bits []byte) int64 {
+    return matchFloat32EqualGeneric(src, val, bits)
+}
+
+func matchFloat32NotEqual(src []float32, val float32, bits []byte) int64 {
+    return matchFloat32NotEqualGeneric(src, val, bits)
+}
+
+func matchFloat32LessThan(src []float32, val float32, bits []byte) int64 {
+    return matchFloat32LessThanGeneric(src, val, bits)
+}
+
+func matchFloat32LessThanEqual(src []float32, val float32, bits []byte) int64 {
+    return matchFloat32LessThanEqualGeneric(src, val, bits)
+}
+
+func matchFloat32GreaterThan(src []float32, val float32, bits []byte) int64 {
+    return matchFloat32GreaterThanGeneric(src, val, bits)
+}
+
+func matchFloat32GreaterThanEqual(src []float32, val float32, bits []byte) int64 {
+    return matchFloat32GreaterThanEqualGeneric(src, val, bits)
+}
+
+func matchFloat32Between(src []float32, a, b float32, bits []byte) int64 {
+    return matchFloat32BetweenGeneric(src, a, b, bits)
+}
--- vec/int128_ref.go
+++ vec/int128_ref.go
@@ -1,34 +1,35 @@
 // Copyright (c) 2020 Blockwatch Data Inc.
 // Author: alex@blockwatch.cc
 
+//go:build !amd64 || appengine || gccgo
 // +build !amd64 appengine gccgo
 
 package vec
 
-func matchInt128Equal(src []Int128, val Int128, bits, mask []byte) int64 {
+func matchInt128Equal(src Int128LLSlice, val Int128, bits, mask []byte) int64 {
 	return matchInt128EqualGeneric(src, val, bits, mask)
 }
 
-func matchInt128NotEqual(src []Int128, val Int128, bits, mask []byte) int64 {
+func matchInt128NotEqual(src Int128LLSlice, val Int128, bits, mask []byte) int64 {
 	return matchInt128NotEqualGeneric(src, val, bits, mask)
 }
 
-func matchInt128LessThan(src []Int128, val Int128, bits, mask []byte) int64 {
+func matchInt128LessThan(src Int128LLSlice, val Int128, bits, mask []byte) int64 {
 	return matchInt128LessThanGeneric(src, val, bits, mask)
 }
 
-func matchInt128LessThanEqual(src []Int128, val Int128, bits, mask []byte) int64 {
+func matchInt128LessThanEqual(src Int128LLSlice, val Int128, bits, mask []byte) int64 {
 	return matchInt128LessThanEqualGeneric(src, val, bits, mask)
 }
 
-func matchInt128GreaterThan(src []Int128, val Int128, bits, mask []byte) int64 {
+func matchInt128GreaterThan(src Int128LLSlice, val Int128, bits, mask []byte) int64 {
 	return matchInt128GreaterThanGeneric(src, val, bits, mask)
 }
 
-func matchInt128GreaterThanEqual(src []Int128, val Int128, bits, mask []byte) int64 {
+func matchInt128GreaterThanEqual(src Int128LLSlice, val Int128, bits, mask []byte) int64 {
 	return matchInt128GreaterThanEqualGeneric(src, val, bits, mask)
 }
 
-func matchInt128Between(src []Int128, a, b Int128, bits, mask []byte) int64 {
+func matchInt128Between(src Int128LLSlice, a, b Int128, bits, mask []byte) int64 {
 	return matchInt128BetweenGeneric(src, a, b, bits, mask)
 }
