823b6be57da3492a899aed60174a1ae6ebdcdd03
Alexander Eichhorn <alex@blockwatch.cc>
2023-10-26 10:42:03 +0400

    Update fnv hash API

--- hash/inline_fnv.go
+++ hash/inline_fnv.go
@@ -1,6 +1,5 @@
 // Copyright (c) 2020 Blockwatch Data Inc.
 // Author: alex@blockwatch.cc
-//
 package hash
 
 import (
@@ -23,19 +22,24 @@ func NewInlineFNV64a() InlineFNV64a {
 }
 
 // Write adds data to the running hash.
-func (s *InlineFNV64a) Write(data []byte) (int, error) {
+func (s *InlineFNV64a) Write(data []byte) error {
 	hash := uint64(*s)
 	for _, c := range data {
 		hash ^= uint64(c)
 		hash *= prime64
 	}
 	*s = InlineFNV64a(hash)
-	return len(data), nil
+	return nil
 }
 
 // Write adds data to the running hash.
-func (s *InlineFNV64a) WriteString(data string) (int, error) {
+func (s *InlineFNV64a) WriteString(data string) error {
 	return s.Write([]byte(data))
+}
+
+// Write adds a single byte b to the running hash.
+func (s *InlineFNV64a) WriteByte(b byte) error {
+	return s.Write([]byte{b})
 }
 
 // Sum64 returns the uint64 of the current resulting hash.
