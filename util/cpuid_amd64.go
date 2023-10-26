7617f12854b4d69a1849b189f1c80febaa9cfd61
Alexander Eichhorn <alex@blockwatch.cc>
2023-10-26 10:26:40 +0400

    Change CPU ident

--- util/cpuid_amd64.go
+++ util/cpuid_amd64.go
@@ -6,20 +6,20 @@
 
 package util
 
-import (
-	"log"
-
-	"golang.org/x/sys/cpu"
-)
+import "golang.org/x/sys/cpu"
 
 //go:noescape
 func cpuid(eaxArg, ecxArg uint32) (eax, ebx, ecx, edx uint32)
 
 func init() {
 	detectAVX()
+	EnableAVX = detectAVX
+	DisableAVX2 = disableAVX2
+	DisableAVX512 = disableAVX512
 }
 
 func detectAVX() {
+	IsAMD64 = true
 	UseAVX2 = cpu.X86.HasAVX2
 
 	maxID, _, _, _ := cpuid(0, 0)
@@ -57,11 +57,11 @@ func isSet(bitpos uint, value uint32) bool {
 	return value&(1<<bitpos) != 0
 }
 
-func DisableAVX2() {
+func disableAVX2() {
 	UseAVX2 = false
 }
 
-func DisableAVX512() {
+func disableAVX512() {
 	UseAVX512_F = false
 	UseAVX512_DQ = false
 	UseAVX512_IFMA = false
@@ -77,48 +77,4 @@ func DisableAVX512() {
 	UseAVX512_4FMAPS = false
 	UseAVX512_VP2INTERSECT = false
 	UseAVX512_BF16 = false
-}
-
-func EnableAVX() {
-	detectAVX()
-}
-
-var (
-	UseAVX2 bool
-
-	UseAVX512_F            bool // AVX-512 Foundation Instructions
-	UseAVX512_DQ           bool // AVX-512 Doubleword & Quadword Instrs
-	UseAVX512_IFMA         bool // AVX-512 Integer Fused Multiply Add
-	UseAVX512_PF           bool // AVX-512 Prefetch Instructions
-	UseAVX512_ER           bool // AVX-512 Exponent & Reciprocal Instrs
-	UseAVX512_CD           bool // AVX-512 Conflict Detection Instrs
-	UseAVX512_BW           bool // AVX-512 Byte and Word Instructions
-	UseAVX512_VL           bool // AVX-512 Vector Length Extensions
-	UseAVX512_VBMI         bool // AVX-512 Vector Byte Manipulation Instrs
-	UseAVX512_BITALG       bool // Support for VPOPCNT[B,W] and VPSHUFBITQMB
-	UseAVX512_VPOPCNTDQ    bool // POPCNT for vectors of DW/QW
-	UseAVX512_4VNNIW       bool // AVX512 Neural Network Instructions
-	UseAVX512_4FMAPS       bool // AVX512 Multiply Accumulation Single Precision
-	UseAVX512_VP2INTERSECT bool // VP2INTERSECT{D,Q} insns
-	UseAVX512_BF16         bool // AVX512 BFloat16 Instructions
-)
-
-func LogCPUFeatures(l *log.Logger) {
-	l.Printf("AMD64 CPU detected")
-	l.Printf(" AVX2 %t", UseAVX2)
-	l.Printf(" AVX512-F %t", UseAVX512_F)
-	l.Printf(" AVX512-DQ %t", UseAVX512_DQ)
-	l.Printf(" AVX512-IFMA %t", UseAVX512_IFMA)
-	l.Printf(" AVX512-PF %t", UseAVX512_PF)
-	l.Printf(" AVX512-ER %t", UseAVX512_ER)
-	l.Printf(" AVX512-CD %t", UseAVX512_CD)
-	l.Printf(" AVX512-BW %t", UseAVX512_BW)
-	l.Printf(" AVX512-VL %t", UseAVX512_VL)
-	l.Printf(" AVX512-VBMI %t", UseAVX512_VBMI)
-	l.Printf(" AVX512-BITALG %t", UseAVX512_BITALG)
-	l.Printf(" AVX512-VPOPCNTDQ %t", UseAVX512_VPOPCNTDQ)
-	l.Printf(" AVX512-4VNNIW %t", UseAVX512_4VNNIW)
-	l.Printf(" AVX512-4FMAPS %t", UseAVX512_4FMAPS)
-	l.Printf(" AVX512-VP2INTERSECT %t", UseAVX512_VP2INTERSECT)
-	l.Printf(" AVX512-BF16 %t", UseAVX512_BF16)
 }
