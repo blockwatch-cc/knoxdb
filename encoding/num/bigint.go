e393b01264de0799848c4cb8e466c70ac448526f
Alexander Eichhorn <alex@blockwatch.cc>
2024-07-24 20:31:06 +0400

    Fix bigint marshal

--- encoding/num/bigint.go
+++ encoding/num/bigint.go
@@ -1,308 +1,307 @@
-// Copyright (c) 2020-2023 Blockwatch Data Inc.
+// Copyright (c) 2020-2024 Blockwatch Data Inc.
 // Author: alex@blockwatch.cc
 
 package num
 
 import (
-    "bytes"
-    "math"
-    "math/big"
-    "strings"
+	"bytes"
+	"math"
+	"math/big"
+	"strings"
 )
 
-// A variable length sequence of bytes representing a Big integer number
-// with unlimited precision.
+// A variable length sequence of bytes representing an unsigned Big integer
+// number with unlimited precision. Negative bigints are unsupported for
+// binary marshaling to enable native comparisons operators on encoded
+// binary data (data is stored as big-endian variable length byte slice).
 type Big big.Int
 
 var BigZero = NewBig(0)
 
 func NewBig(i int64) Big {
-    var z Big
-    z.SetInt64(i)
-    return z
+	var z Big
+	z.SetInt64(i)
+	return z
 }
 
 func NewFromBigInt(b *big.Int) Big {
-    var z Big
-    z.SetBig(b)
-    return z
+	var z Big
+	z.SetBig(b)
+	return z
 }
 
 func (z Big) Big() *big.Int {
-    return (*big.Int)(&z)
+	return (*big.Int)(&z)
 }
 
 func (z Big) Equal(x Big) bool {
-    return z.Big().Cmp(x.Big()) == 0
+	return z.Big().Cmp(x.Big()) == 0
 }
 
 func (z Big) IsBigZero() bool {
-    return len((*big.Int)(&z).Bits()) == 0
+	return len((*big.Int)(&z).Bits()) == 0
 }
 
 func (z Big) Cmp(b Big) int {
-    return (*big.Int)(&z).Cmp((*big.Int)(&b))
+	return (*big.Int)(&z).Cmp((*big.Int)(&b))
 }
 
 func BigCmp(a, b Big) int {
-    return a.Cmp(b)
+	return a.Cmp(b)
 }
 
 func (z Big) IsLess(b Big) bool {
-    return z.Cmp(b) < 0
+	return z.Cmp(b) < 0
 }
 
 func (z Big) IsLessEqual(b Big) bool {
-    return z.Cmp(b) <= 0
+	return z.Cmp(b) <= 0
 }
 
 func (z Big) Int64() int64 {
-    return (*big.Int)(&z).Int64()
+	return (*big.Int)(&z).Int64()
 }
 
 func (z *Big) SetBig(b *big.Int) *Big {
-    (*big.Int)(z).Set(b)
-    return z
+	(*big.Int)(z).Set(b)
+	return z
 }
 
 func (z *Big) SetInt64(i int64) *Big {
-    (*big.Int)(z).SetInt64(i)
-    return z
+	(*big.Int)(z).SetInt64(i)
+	return z
 }
 
 func (z Big) Clone() Big {
-    var x Big
-    x.SetBig(z.Big())
-    return x
+	var x Big
+	x.SetBig(z.Big())
+	return x
 }
 
 func (z *Big) UnmarshalBinary(buf []byte) error {
-    return (*big.Int)(z).GobDecode(buf)
+	(*big.Int)(z).SetBytes(buf)
+	return nil
 }
 
 func (z *Big) DecodeBuffer(buf *bytes.Buffer) error {
-    return (*big.Int)(z).GobDecode(buf.Bytes())
+	(*big.Int)(z).SetBytes(buf.Bytes())
+	return nil
+}
+
+func (z Big) Bytes() []byte {
+	return (*big.Int)(&z).Bytes()
 }
 
 func (z Big) MarshalBinary() ([]byte, error) {
-    return (*big.Int)(&z).GobEncode()
+	return z.Bytes(), nil
 }
 
 func (z Big) EncodeBuffer(buf *bytes.Buffer) error {
-    b, err := (*big.Int)(&z).GobEncode()
-    if err != nil {
-        return err
-    }
-    buf.Write(b)
-    return nil
+	buf.Write(z.Bytes())
+	return nil
 }
 
 func ParseBig(s string) (Big, error) {
-    var z Big
-    err := (*big.Int)(&z).UnmarshalText([]byte(s))
-    return z, err
+	var z Big
+	err := (*big.Int)(&z).UnmarshalText([]byte(s))
+	return z, err
 }
 
 func MustParseBig(s string) Big {
-    z, err := ParseBig(s)
-    if err != nil {
-        panic(err)
-    }
-    return z
+	z, err := ParseBig(s)
+	if err != nil {
+		panic(err)
+	}
+	return z
 }
 
 // Set implements the flags.Value interface for use in command line argument parsing.
 func (z *Big) Set(val string) (err error) {
-    *z, err = ParseBig(val)
-    return
+	*z, err = ParseBig(val)
+	return
 }
 
 func (z Big) MarshalText() ([]byte, error) {
-    return (*big.Int)(&z).MarshalText()
+	return (*big.Int)(&z).MarshalText()
 }
 
 func (z *Big) UnmarshalText(d []byte) error {
-    return (*big.Int)(z).UnmarshalText(d)
+	return (*big.Int)(z).UnmarshalText(d)
 }
 
 func (z Big) String() string {
-    return (*big.Int)(&z).Text(10)
-}
-
-func (z Big) Bytes() []byte {
-    buf, _ := z.MarshalBinary()
-    return buf
+	return (*big.Int)(&z).Text(10)
 }
 
 func (z Big) Decimals(d int) string {
-    s := z.String()
-    if d <= 0 {
-        return s
-    }
-    var sig string
-    if z.IsNeg() {
-        sig = "-"
-        s = s[1:]
-    }
-    l := len(s)
-    if l <= d {
-        s = strings.Repeat("0", d-l+1) + s
-    }
-    l = len(s)
-    return sig + s[:l-d] + "." + s[l-d:]
-}
-
-func (z Big) Neg() Big {
-    var n Big
-    n.SetBig(new(big.Int).Neg(z.Big()))
-    return n
-}
-
-func (z Big) Add(y Big) Big {
-    var x Big
-    x.SetBig(new(big.Int).Add(z.Big(), y.Big()))
-    return x
-}
-
-func (z Big) Sub(y Big) Big {
-    var x Big
-    x.SetBig(new(big.Int).Sub(z.Big(), y.Big()))
-    return x
-}
-
-func (z Big) Mul(y Big) Big {
-    var x Big
-    x.SetBig(new(big.Int).Mul(z.Big(), y.Big()))
-    return x
-}
-
-func (z Big) Div(y Big) Big {
-    var x Big
-    if !y.IsBigZero() {
-        x.SetBig(new(big.Int).Div(z.Big(), y.Big()))
-    }
-    return x
-}
-
-func (z Big) CeilDiv(y Big) Big {
-    var x Big
-    if !y.IsBigZero() {
-        d, m := new(big.Int).DivMod(z.Big(), y.Big(), new(big.Int))
-        x.SetBig(d)
-        x = x.Add64(int64(m.Cmp(BigZero.Big())))
-    }
-    return x
-}
-
-func (z Big) Add64(y int64) Big {
-    var x Big
-    x.SetBig(new(big.Int).Add(z.Big(), big.NewInt(y)))
-    return x
-}
-
-func (z Big) Sub64(y int64) Big {
-    var x Big
-    x.SetBig(new(big.Int).Sub(z.Big(), big.NewInt(y)))
-    return x
-}
-
-func (z Big) Mul64(y int64) Big {
-    var x Big
-    x.SetBig(new(big.Int).Mul(z.Big(), big.NewInt(y)))
-    return x
-}
-
-func (z Big) Div64(y int64) Big {
-    var x Big
-    if y != 0 {
-        x.SetBig(new(big.Int).Div(z.Big(), big.NewInt(y)))
-    }
-    return x
+	s := z.String()
+	if d <= 0 {
+		return s
+	}
+	var sig string
+	if z.IsNeg() {
+		sig = "-"
+		s = s[1:]
+	}
+	l := len(s)
+	if l <= d {
+		s = strings.Repeat("0", d-l+1) + s
+	}
+	l = len(s)
+	return sig + s[:l-d] + "." + s[l-d:]
 }
 
 func (z Big) IsNeg() bool {
-    return z.Big().Sign() < 0
+	return z.Big().Sign() < 0
+}
+
+func (z Big) Neg() Big {
+	var n Big
+	n.SetBig(new(big.Int).Neg(z.Big()))
+	return n
+}
+
+func (z Big) Add(y Big) Big {
+	var x Big
+	x.SetBig(new(big.Int).Add(z.Big(), y.Big()))
+	return x
+}
+
+func (z Big) Sub(y Big) Big {
+	var x Big
+	x.SetBig(new(big.Int).Sub(z.Big(), y.Big()))
+	return x
+}
+
+func (z Big) Mul(y Big) Big {
+	var x Big
+	x.SetBig(new(big.Int).Mul(z.Big(), y.Big()))
+	return x
+}
+
+func (z Big) Div(y Big) Big {
+	var x Big
+	if !y.IsBigZero() {
+		x.SetBig(new(big.Int).Div(z.Big(), y.Big()))
+	}
+	return x
+}
+
+func (z Big) CeilDiv(y Big) Big {
+	var x Big
+	if !y.IsBigZero() {
+		d, m := new(big.Int).DivMod(z.Big(), y.Big(), new(big.Int))
+		x.SetBig(d)
+		x = x.Add64(int64(m.Cmp(BigZero.Big())))
+	}
+	return x
+}
+
+func (z Big) Add64(y int64) Big {
+	var x Big
+	x.SetBig(new(big.Int).Add(z.Big(), big.NewInt(y)))
+	return x
+}
+
+func (z Big) Sub64(y int64) Big {
+	var x Big
+	x.SetBig(new(big.Int).Sub(z.Big(), big.NewInt(y)))
+	return x
+}
+
+func (z Big) Mul64(y int64) Big {
+	var x Big
+	x.SetBig(new(big.Int).Mul(z.Big(), big.NewInt(y)))
+	return x
+}
+
+func (z Big) Div64(y int64) Big {
+	var x Big
+	if y != 0 {
+		x.SetBig(new(big.Int).Div(z.Big(), big.NewInt(y)))
+	}
+	return x
 }
 
 func (z Big) Scale(n int) Big {
-    var x Big
-    if n == 0 {
-        x.SetBig(z.Big())
-    } else {
-        if n < 0 {
-            factor := new(big.Int).Exp(big.NewInt(10), big.NewInt(int64(-n)), nil)
-            x.SetBig(factor.Div(z.Big(), factor))
-        } else {
-            factor := new(big.Int).Exp(big.NewInt(10), big.NewInt(int64(n)), nil)
-            x.SetBig(factor.Mul(z.Big(), factor))
-        }
-    }
-    return x
+	var x Big
+	if n == 0 {
+		x.SetBig(z.Big())
+	} else {
+		if n < 0 {
+			factor := new(big.Int).Exp(big.NewInt(10), big.NewInt(int64(-n)), nil)
+			x.SetBig(factor.Div(z.Big(), factor))
+		} else {
+			factor := new(big.Int).Exp(big.NewInt(10), big.NewInt(int64(n)), nil)
+			x.SetBig(factor.Mul(z.Big(), factor))
+		}
+	}
+	return x
 }
 
 func (z Big) CeilScale(n int) Big {
-    var x Big
-    if n == 0 {
-        x.SetBig(z.Big())
-    } else {
-        if n < 0 {
-            factor := new(big.Int).Exp(big.NewInt(10), big.NewInt(int64(-n)), nil)
-            f, m := factor.DivMod(z.Big(), factor, new(big.Int))
-            x.SetBig(f)
-            x = x.Add64(int64(m.Cmp(BigZero.Big())))
-        } else {
-            factor := new(big.Int).Exp(big.NewInt(10), big.NewInt(int64(n)), nil)
-            x.SetBig(factor.Mul(z.Big(), factor))
-        }
-    }
-    return x
+	var x Big
+	if n == 0 {
+		x.SetBig(z.Big())
+	} else {
+		if n < 0 {
+			factor := new(big.Int).Exp(big.NewInt(10), big.NewInt(int64(-n)), nil)
+			f, m := factor.DivMod(z.Big(), factor, new(big.Int))
+			x.SetBig(f)
+			x = x.Add64(int64(m.Cmp(BigZero.Big())))
+		} else {
+			factor := new(big.Int).Exp(big.NewInt(10), big.NewInt(int64(n)), nil)
+			x.SetBig(factor.Mul(z.Big(), factor))
+		}
+	}
+	return x
 }
 
 func (z Big) Float64(dec int) float64 {
-    f64, _ := new(big.Float).SetInt(z.Big()).Float64()
-    switch {
-    case dec == 0:
-        return f64
-    case dec < 0:
-        factor := math.Pow10(-dec)
-        return f64 / factor
-    default:
-        factor := math.Pow10(dec)
-        return f64 * factor
-    }
+	f64, _ := new(big.Float).SetInt(z.Big()).Float64()
+	switch {
+	case dec == 0:
+		return f64
+	case dec < 0:
+		factor := math.Pow10(-dec)
+		return f64 / factor
+	default:
+		factor := math.Pow10(dec)
+		return f64 * factor
+	}
 }
 
 func (z Big) Lsh(n uint) Big {
-    return NewFromBigInt(new(big.Int).Lsh(z.Big(), n))
+	return NewFromBigInt(new(big.Int).Lsh(z.Big(), n))
 }
 
 func (z Big) Rsh(n uint) Big {
-    return NewFromBigInt(new(big.Int).Rsh(z.Big(), n))
+	return NewFromBigInt(new(big.Int).Rsh(z.Big(), n))
 }
 
 func MaxBig(args ...Big) Big {
-    var m Big
-    for _, z := range args {
-        if m.Cmp(z) < 0 {
-            m = z
-        }
-    }
-    return m
+	var m Big
+	for _, z := range args {
+		if m.Cmp(z) < 0 {
+			m = z
+		}
+	}
+	return m
 }
 
 func MinBig(args ...Big) Big {
-    switch len(args) {
-    case 0:
-        return Big{}
-    case 1:
-        return args[0]
-    default:
-        m := args[0]
-        for _, z := range args[1:] {
-            if m.Cmp(z) > 0 {
-                m = z
-            }
-        }
-        return m
-    }
+	switch len(args) {
+	case 0:
+		return Big{}
+	case 1:
+		return args[0]
+	default:
+		m := args[0]
+		for _, z := range args[1:] {
+			if m.Cmp(z) > 0 {
+				m = z
+			}
+		}
+		return m
+	}
 }
--- encoding/num/bigint_test.go
+++ encoding/num/bigint_test.go
@@ -1,159 +1,209 @@
-// Copyright (c) 2022 Blockwatch Data Inc.
-// Author: stefan@blockwatch.cc
+// Copyright (c) 2024 Blockwatch Data Inc.
+// Author: alex@blockwatch.cc
 
 package num
 
 import (
-    "bytes"
-    "io"
-    "math/big"
-    "math/rand"
-    "testing"
+	"bytes"
+	"math/rand"
+	"testing"
 )
 
 type BigintDecodeTest struct {
-    name string
-    buf  []byte
-    res  []uint64
-    sign int
-    err  error
+	name string
+	buf  []byte
+	val  string
+	err  error
 }
 
 var bigintDecodeCases = []BigintDecodeTest{
-    {
-        name: "e0",
-        buf:  []byte{},
-        err:  io.ErrShortBuffer,
-    },
-    {
-        name: "e1",
-        buf:  []byte{0xc0},
-        err:  io.ErrShortBuffer,
-    },
-    {
-        name: "e9",
-        buf:  []byte{0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0},
-        err:  io.ErrShortBuffer,
-    },
-    {
-        name: "l1",
-        buf:  []byte{0x20},
-        res:  []uint64{0x20},
-    },
-    {
-        name: "l9",
-        buf:  []byte{0xa0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0x40},
-        res:  []uint64{0x2040810204081020},
-    },
-    {
-        name: "l10",
-        buf:  []byte{0xa0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0x40},
-        res:  []uint64{0x10, 0x2040810204081020},
-    },
-    {
-        name: "l18",
-        buf:  []byte{0xa0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0x40},
-        res:  []uint64{0x1020408102040810, 0x2040810204081020},
-    },
-    {
-        name: "l19",
-        buf:  []byte{0xa0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0x40},
-        res:  []uint64{0x08, 0x1020408102040810, 0x2040810204081020},
-    },
-    {
-        name: "n1",
-        buf:  []byte{0x60},
-        res:  []uint64{0x20},
-        sign: 1,
-    },
-    {
-        name: "n9",
-        buf:  []byte{0xe0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0x40},
-        res:  []uint64{0x2040810204081020},
-        sign: 1,
-    },
-    {
-        name: "n10",
-        buf:  []byte{0xe0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0x40},
-        res:  []uint64{0x10, 0x2040810204081020},
-        sign: 1,
-    },
-    {
-        name: "n18",
-        buf:  []byte{0xe0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0x40},
-        res:  []uint64{0x1020408102040810, 0x2040810204081020},
-        sign: 1,
-    },
-    {
-        name: "n19",
-        buf:  []byte{0xe0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0x40},
-        res:  []uint64{0x08, 0x1020408102040810, 0x2040810204081020},
-        sign: 1,
-    },
+	{
+		name: "l0",
+		buf:  []byte{},
+		val:  "0",
+	},
+	{
+		name: "l1",
+		buf:  []byte{0x20},
+		val:  "32",
+	},
+	{
+		name: "l9",
+		buf:  []byte{0x20, 0x40, 0x81, 0x02, 0x04, 0x08, 0x10, 0x20},
+		val:  "2323999253380730912",
+	},
+	{
+		name: "l10",
+		buf:  []byte{0x10, 0x20, 0x40, 0x81, 0x02, 0x04, 0x08, 0x10, 0x20},
+		val:  "297471904432733556768",
+	},
+	{
+		name: "l18",
+		buf:  []byte{0x10, 0x20, 0x40, 0x81, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x81, 0x02, 0x04, 0x08, 0x10, 0x20},
+		val:  "21435109727303210296905487082316107808",
+	},
+	{
+		name: "l19",
+		buf:  []byte{0x08, 0x10, 0x20, 0x40, 0x81, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x81, 0x02, 0x04, 0x08, 0x10, 0x20},
+		val:  "2743694045094810918003902346536461799456",
+	},
+
+	// negative bigints are not supported
+	// {
+	// 	name: "n1",
+	// 	buf:  []byte{0x20},
+	//
+	// 	sign: 1,
+	// 	val:  "-32",
+	// },
+	// {
+	// 	name: "n9",
+	// 	buf:  []byte{0xe0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0x40},
+	//
+	// 	sign: 1,
+	// 	val:  "-2323999253380730912",
+	// },
+	// {
+	// 	name: "n10",
+	// 	buf:  []byte{0xe0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0x40},
+	//
+	// 	sign: 1,
+	// 	val:  "-297471904432733556768",
+	// },
+	// {
+	// 	name: "n18",
+	// 	buf:  []byte{0xe0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0x40},
+	//
+	// 	sign: 1,
+	// 	val:  "-21435109727303210296905487082316107808",
+	// },
+	// {
+	// 	name: "n19",
+	// 	buf:  []byte{0xe0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0x40},
+	//
+	// 	sign: 1,
+	// 	val:  "-2743694045094810918003902346536461799456",
+	// },
 }
 
-func TestDecodeBuffer(t *testing.T) {
-    for _, c := range bigintDecodeCases {
-        var z Big
-        err := z.DecodeBuffer(bytes.NewBuffer(c.buf))
-        if got, want := err, c.err; got != want {
-            t.Errorf("%s: unexpected error %v, expected %v", c.name, got, want)
-        }
-        if err != nil {
-            continue
-        }
-        res := big.NewInt(0)
-        n := new(big.Int)
-        for _, v := range c.res {
-            n.SetUint64(v)
-            res.Or(res.Lsh(res, 64), n)
-        }
-        if c.sign != 0 {
-            res.Neg(res)
-        }
-        if got, want := z, (Big)(*res); got.Cmp(want) != 0 {
-            t.Errorf("%s: unexpected result %v, expected %v", c.name, got, want)
-        }
-    }
+func TestBigCompare(t *testing.T) {
+	for _, v := range [][2]int64{
+		{1 << 8, 1<<8 + 1},
+		{1 << 16, 1<<16 + 1},
+		{1 << 32, 1<<32 + 1},
+	} {
+		var x, y Big
+		x.SetInt64(v[0])
+		y.SetInt64(v[1])
+		if got, want := x.IsLess(y), v[0] < v[1]; got != want {
+			t.Errorf("%d ? %d: unexpected cmp order", v[0], v[1])
+		}
+
+		cmp := bytes.Compare(x.Bytes(), y.Bytes())
+		if got, want := cmp < 0, v[0] < v[1]; got != want {
+			t.Errorf("%d ? %d: unexpected bytes cmp order", v[0], v[1])
+		}
+	}
+}
+
+func TestBigTextMarshal(t *testing.T) {
+	for _, c := range bigintDecodeCases {
+		var x Big
+		err := x.UnmarshalBinary(c.buf)
+		if err != nil {
+			t.Errorf("%s: unexpected binary unmarshal error %v", c.name, err)
+		}
+
+		var z Big
+		err = z.UnmarshalText([]byte(c.val))
+		if got, want := err, c.err; got != want {
+			t.Errorf("%s: unexpected error %v, expected %v", c.name, got, want)
+		}
+		if err != nil {
+			continue
+		}
+		if got, want := z, x; got.Cmp(want) != 0 {
+			t.Errorf("%s: unexpected result %v, expected %v", c.name, got, want)
+		}
+
+		buf, err := z.MarshalText()
+		if err != nil {
+			t.Errorf("%s: unexpected text marshal error %v", c.name, err)
+		}
+		if got, want := string(buf), c.val; got != want {
+			t.Errorf("%s: unexpected text %s, expected %s", c.name, got, want)
+		}
+	}
+}
+
+func TestBigBinaryMarshal(t *testing.T) {
+	for _, c := range bigintDecodeCases {
+		var x Big
+		err := x.UnmarshalText([]byte(c.val))
+		if err != nil {
+			t.Errorf("%s: unexpected text unmarshal error %v", c.name, err)
+		}
+
+		var z Big
+		err = z.UnmarshalBinary(c.buf)
+		if got, want := err, c.err; got != want {
+			t.Errorf("%s: unexpected error %v, expected %v", c.name, got, want)
+		}
+		if err != nil {
+			continue
+		}
+
+		if got, want := z, x; got.Cmp(want) != 0 {
+			t.Errorf("%s: unexpected result %v, expected %v", c.name, got, want)
+		}
+
+		buf, err := z.MarshalBinary()
+		if err != nil {
+			t.Errorf("%s: unexpected binary marshal error %v", c.name, err)
+		}
+		if got, want := buf, c.buf; !bytes.Equal(got, want) {
+			t.Errorf("%s: unexpected binary %v, expected %v", c.name, got, want)
+		}
+	}
 }
 
 type benchmarkSize struct {
-    name string
-    l    int
+	name string
+	l    int
 }
 
 var benchmarkSizes = []benchmarkSize{
-    {"6bit", 1},
-    {"62bit", 9},
-    {"125bit", 18},
-    {"251bit", 36},
-    {"510bit", 73},
+	{"6bit", 1},
+	{"62bit", 9},
+	{"125bit", 18},
+	{"251bit", 36},
+	{"510bit", 73},
 }
 
 func randBigintSlice(n int) []byte {
-    s := make([]byte, n)
-    if n == 1 {
-        s[0] = byte(rand.Intn(0x40))
-        return s
-    }
+	s := make([]byte, n)
+	if n == 1 {
+		s[0] = byte(rand.Intn(0x40))
+		return s
+	}
 
-    s[0] = byte(rand.Intn(0x40)) | 0x80
-    for i := 1; i < n-1; i++ {
-        s[i] = byte(rand.Intn(0x80)) | 0x80
-    }
-    s[n-1] = byte(rand.Intn(0x80))
-    return s
+	s[0] = byte(rand.Intn(0x40)) | 0x80
+	for i := 1; i < n-1; i++ {
+		s[i] = byte(rand.Intn(0x80)) | 0x80
+	}
+	s[n-1] = byte(rand.Intn(0x80))
+	return s
 }
 
-func BenchmarkDecodeBuffer(b *testing.B) {
-    for _, bm := range benchmarkSizes {
-        buf := randBigintSlice(bm.l)
-        var z Big
-        b.Run(bm.name, func(b *testing.B) {
-            b.SetBytes(int64(bm.l))
-            for i := 0; i < b.N; i++ {
-                z.DecodeBuffer(bytes.NewBuffer(buf))
-            }
-        })
-    }
+func BenchmarkBigUnmarshalBinary(b *testing.B) {
+	for _, bm := range benchmarkSizes {
+		buf := randBigintSlice(bm.l)
+		var z Big
+		b.Run(bm.name, func(b *testing.B) {
+			b.SetBytes(int64(bm.l))
+			for i := 0; i < b.N; i++ {
+				z.UnmarshalBinary(buf)
+			}
+		})
+	}
 }
