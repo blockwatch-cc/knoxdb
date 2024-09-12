9f269e81e3b1d7fb7e70b4de5ecca03be80e3397
Alexander Eichhorn <alex@blockwatch.cc>
2024-09-12 12:28:13 +0400

    v2: add logger to default opts

--- pkg/knox/options.go
+++ pkg/knox/options.go
@@ -3,18 +3,23 @@
 
 package knox
 
-import "blockwatch.cc/knoxdb/internal/engine"
+import (
+	"blockwatch.cc/knoxdb/internal/engine"
+	"github.com/echa/log"
+)
 
 var (
 	DefaultDatabaseOptions = engine.DatabaseOptions{
 		PageSize:  1024,
 		PageFill:  0.8,
 		CacheSize: 16 * 1 << 20,
+		Logger:    log.New(nil).SetLevel(log.LevelInfo),
 	}
 	ReadonlyDatabaseOptions = engine.DatabaseOptions{
 		PageSize:  1024,
 		PageFill:  0.8,
 		CacheSize: 16 * 1 << 20,
 		ReadOnly:  true,
+		Logger:    log.New(nil).SetLevel(log.LevelInfo),
 	}
 )
