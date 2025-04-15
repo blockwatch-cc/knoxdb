265bf54707960f4ea0015f6a40ceeb134843b6ea
Oliver Simon <oliver@blockwatch.cc>
2025-04-15 18:19:51 +0900

    v2: add run bench

---
+++ internal/fcbench/cmd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/blockwatch-cc/knoxdb/encode/fcbench"
+	"log"
+)
+
+func main() {
+	cfg := fcbench.BenchmarkConfig{
+		VectorLengths: []int{128, 256, 512, 1024},
+		DatasetSize:   1000000,
+		DatasetType:   "timeseries",
+		OutputDir:     "./bench_results",
+		Encoders:      []string{"delta", "gorilla", "rle"},
+		Repeat:        3,
+	}
+	results, err := fcbench.RunBenchmarks(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, r := range results {
+		log.Printf("Encoder: %s, Vector: %d, Ratio: %.2f, Throughput: %.2f val/s",
+			r.EncoderName, r.VectorLength, r.CompressionRatio, r.Throughput)
+	}
+}
